f44475b21b19de7ded0154df2dcd3a446b79a424
Varga Márk <varga.mark3@gmail.com>
2020-07-31 15:20:28 +0200

    added methods

--- client/client.js
+++ client/client.js
@@ -1,6 +1,21 @@
+const API_URL = "http://localhost:5000/search/";
+
 const app = new Vue({
   el: "#app",
   data: {
-    tags: [],
+    term: "",
+    terms: [],
+  },
+  methods: {
+    onSubmit() {
+      this.terms.push(this.term);
+      const url = `${API_URL}${this.term}`;
+
+      fetch(url)
+        .then((response) => response.json())
+        .then((json) => {
+          console.log(json);
+        });
+    },
   },
 });
